6b1972003717c8b6b7fe0d2adc905275b0ff9c42
mac_subing <v_subing@163.com>
2024-12-24 16:43:34 +0800

    ssh文件修正

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ cp target/*.jar deploy_tmp/target/app.jar
 cp Dockerfile deploy_tmp/
 cp docker-compose.yml deploy_tmp/
 cp -r nginx deploy_tmp/
-cp -r ssl deploy_tmp/
+# cp -r ssl deploy_tmp/
 mkdir -p deploy_tmp/logs
 
 # 打包文件
